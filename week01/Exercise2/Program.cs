bfe562b81381b26ad736db06dd0e9f410ec3e0a1
CooperSandra <scooper1@byupathway.edu>
2025-09-12 22:27:57 -0400

    Exercise 1, 2 and 3 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask the user for their grade percentage
+        Console.WriteLine("\nEnter your grade percentage (ex. 75): ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        // Initialize a variable to hold the letter grade
+        string letterGrade = " ";
+
+        // Determine the letter grade based on the percentage
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        // Display the letter grade to the user
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        // Provide feedback based on the grade percentage
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Keep trying, you've got this!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Display a greeting message to the user
+        //Console.WriteLine("\nWhat is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); // Generates a random number between 1 and 100
+
+        int guess = -1;
+
+        // Prompt the user to guess the magic number
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("Guess the magic number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            // Check if the user's guess is correct and provide feedback
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low - try again!\n");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high - try again!\n");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number!");
+            }
+        }
     }
 }
